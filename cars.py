1c611477c445371eb7d1d749574e27de04990839
JoDeMiro <joedemiro@pintye.com>
2022-03-20 11:33:44 +0100

    add cars modul

--- cars.py
+++ cars.py
@@ -1,3 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import MinMaxScaler
+
+import os
+import pickle
+
+
 class TestCar():
   def __init__(self, road, plotter, storage):
     self.plot_frequency = 9
