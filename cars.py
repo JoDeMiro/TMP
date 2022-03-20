8e6a16ac2e299f6e6a717fa930ea7461a00f1811
JoDeMiro <joedemiro@pintye.com>
2022-03-20 15:44:13 +0100

    update Car to cars.py

--- cars.py
+++ cars.py
@@ -157,9 +157,9 @@ class TestCar():
 
       print(' --------------- plot --------------- ')
 
-  def plot_history_fixed(self, flag, ymin, ymax, height):
+  def plot_history_fixed(self, flag, ymin, ymax, width, height):
     if( flag != 0 ):
-      fig, ax = self.road.show(height)
+      fig, ax = self.road.show(width, height)
       circle = plt.Circle((self.x, self.y), 5, color='black')
       ax.add_patch(circle)
       # v.24 - add standardized color -> left = green, rigth = orange
