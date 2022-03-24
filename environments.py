0ba894b3efe29558d091dc8958194cb569d20acb
JoDeMiro <joedemiro@pintye.com>
2022-03-24 13:07:14 +0100

    more road controll environment.py

--- environments.py
+++ environments.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Road():
-  def __init__(self, wide, length, type=1, v=124, shift=0):
+  def __init__(self, wide, length, type=1, v=124, shift=0, strech=0):
     self.shift       = shift  # 0
+    self.strech      = strech # 0
     self.length      = length # 3000
     self.distance    = np.arange(0, self.length, 1)
     self.wide        = wide
@@ -29,6 +30,25 @@ class Road():
         self.wall_right[0:100] = self.wall_right[101]
         self.wall_center[0:100] = self.wall_center[101]
         self.wall_left[0:100] = self.wall_left[101]
+
+    # Azt kell megcsinálni, hogy az egyik utat kicsit jobban nyújtsa, vagyis lasabb legyen a ciklusa, az út közepe pedig a másik faltól fix
+
+    if(type == 98):
+        def func(x, strech):
+            f = 30*(np.sin(x/180 + strech)) + x * 0.3 + 30 * np.cos(x/30) + 50 * np.sin(x/90)
+            return f
+        self.length      = length # 3000
+        self.distance    = np.arange(0, self.length, 1)
+        self.wall_right  = func(self.distance)
+        # A wall_left-et kell megnyújtani
+        self.wall_left   = func(self.distance, self.strech) + self.wide
+        # A center pedig nem a kettő átlaga legyen, hanem a self.wall_right + (self.wide / 2)
+        # self.wall_center = ( self.wall_left + self.wall_right ) / 2
+        self.wall_center = self.wall_right + (self.wide / 2)
+        self.wall_right[0:100] = self.wall_right[101]
+        self.wall_center[0:100] = self.wall_center[101]
+        self.wall_left[0:100] = self.wall_left[101]
+
 
 
 
